88e78a700e7880c85dcd2105ecf6a9169d3be07d
Alexandre Amory <amamory@gmail.com>
2020-06-26 21:13:20 -0300

    including a vhdl version of axis_m_const

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #export VIVADO=/opt/Xilinx/Vivado/2018.2/bin/vivado
 export VIVADO=/home/lsa/xilinx/2018.2/Vivado/2018.2/bin/vivado
-export VIVADO_DESIGN_NAME=axis_s_const
+export VIVADO_DESIGN_NAME=axis_m_const
 #export VIVADO_TOP_NAME=$VIVADO_DESIGN_NAME
 
 if [ -f $VIVADO ]; then
