78636bae9a35f38a92ac808cbbf7ef855105f496
Alexandre Amory <amamory@gmail.com>
2020-07-29 18:23:54 -0300

    including dont touch attributes so that the logic is not optimized during synthesis

--- hw/hdl/axis_m_const_ip.vhd
+++ hw/hdl/axis_m_const_ip.vhd
@@ -1,4 +1,4 @@
-
+-- module used to connect to the unused slave Hermes router ports so that they are not optmized 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -14,9 +14,32 @@ Port (
 end axis_m_const;
 
 architecture Behavioral of axis_m_const is
+
+signal data_s : std_logic_vector(31 downto 0);
+signal valid_s : std_logic;
+signal ready_s : std_logic;
+
+attribute dont_touch : string;
+attribute dont_touch of data_s : signal is "true";
+
+attribute dont_touch : string;
+attribute dont_touch of valid_s : signal is "true";
+
+attribute dont_touch : string;
+attribute dont_touch of ready_s : signal is "true";
+
 begin
 
-valid_o <= '0';
-data_o  <= (others => '0');
+process(clock)
+begin
+	if rising_edge(clock) then
+		data_s <= (others => '0');
+		valid_s <= '0';
+		ready_s <= ready_i;
+	end if;
+end process;
+
+valid_o <= valid_s;
+data_o  <= data_s;
 
 end architecture;
